b8358e880d9d3b39b2469b7b2b9dd40cef589570
Riko Yasuda <rikomintea1827@gmail.com>
2021-05-23 11:48:40 +0900

    Initial Commit

---
+++ FavRecipe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FavRecipe
+//
+//  Created by スマート・ナビ on 2021/05/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
